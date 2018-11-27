f01d33223c374bf507ec9e5e46fa0e3c67e8c088
Seth Donaldson <sethd63@gmail.com>
2018-11-27 13:15:54 -0700

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Seth Donaldson on 11/27/18.
+//  Copyright © 2018 Seth Donaldson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
