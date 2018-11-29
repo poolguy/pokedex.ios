3e4de89e17cbda8e87a5e2cc18954c9d547f20ac
Seth Donaldson <sethdonaldson@Seths-MacBook-Pro.local>
2018-11-28 21:15:32 -0700

    A not working pokedex

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Seth Donaldson on 11/27/18.
+//  Copyright © 2018 Seth Donaldson. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    var name: String?
+    var url: String?
+}
--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import Foundation
+// import PokemonKit
 
 class ViewController: UIViewController {
-
+    private var pokemonList = PokemonList()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pokemonList.requestData { [weak self] (data: String) in
+            self?.useData(data: data)
+        }
+        
     }
+    
+    private func useData(data: String) {
+        print(data)
+    }
+    
+    class ViewController: UITableViewController {
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return pokemonList.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath) as! UITableViewCell
+            cell.textLabel?.text = "test"
+            return cell
+        }
+    }
+    
+}
 
 
+
+
+class PokemonList {
+    var name: String?
+    var url: String?
+    
+    func requestData(completion: @escaping ((_ data: String) -> Void)) {
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/")
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    
+                    if let json = jsonSerialized, let count = json["count"], let results = json["results"] {
+                        print(count)
+                        print(results)
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            completion(data as! String)
+        }
+        task.resume()
+        
+    }
 }
 
